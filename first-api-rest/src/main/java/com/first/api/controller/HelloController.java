0267a22e4fb4ef80ee9cff63b97e6be3ea74f758
Your Name <you@example.com>
2025-10-15 16:51:57 -0600

    Nuevos cambios

--- first-api-rest/src/main/java/com/first/api/controller/HelloController.java
+++ first-api-rest/src/main/java/com/first/api/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+#Este el controler1
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
